bb213bae5dc7e11dd4b2eb8d6b9dd23b0997e0a8
Omari Pratt <omari.pratt@hgs.hiddengeniusproject.org>
2022-01-21 00:56:06 +0000

    created a variable literals program

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    String name = "Omari";
+    int myNum = 14;
+    String dreamjob = "Pilot"; 
+    
+System.out.println("My name is " + name + ". I am " + (myNum) + " years old. When I grow up I want to be a " + dreamjob +".");
+
+  }
+}
